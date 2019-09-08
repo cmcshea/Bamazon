c4728f3a4b41974052594e5f5ea90dd105a54334
cmcshea <cory.mcshea@gmail.com>
2019-09-07 23:34:03 -0400

    adding view products table for Manager

--- bamazonManager.js
+++ bamazonManager.js
@@ -20,15 +20,34 @@ connection.connect(function (err) {
     menuOptions();
 });
 
+//Asking the manager to choose an option
+
 function menuOptions() {
     inquirer.prompt([{
         type: "list",
         name: "menu",
         message: "What would you like to do?",
         choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
-    }]);
+    }]).then(function (answers) {
+        switch(answers.menu){
+        case "View Products for Sale": productsForSale();
+        break;
+        case "View Low Inventory": lowInventory();
+        break;
+        case "Add to Inventory": addInventory();
+        break;
+        case "Add New Product": addProduct();
+        }
+    });
+};
 
-}
+function productsForSale(){
+        connection.query("SELECT * FROM products", function (err, data) {
+        if (err) throw err;
+        console.table(data)
+        // BuyOrSell(data)
+    });
+};
 
 // function runSearch() {
 //     connection.query("SELECT * FROM products", function (err, data) {
